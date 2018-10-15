ce428b28d32ccc86d4ee37a9e320ec1318bfa266
Zoviet <ulgrad@inbox.ru>
2018-10-15 14:36:39 +0400

    min

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,22 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var debug = require('gulp-debug');
+var rename = require('gulp-rename');
+var babel = require('gulp-babel');
 var config = {
-    srcDir: '',
-    devDir: './min',
+    srcDir: './src',
+    devDir: './dist',
     jsPattern: '*.js', 
     jsDir: ''
 };
 
 
 gulp.task('js', function () {
-    return gulp.src(config.jsPattern)      
-    .pipe(uglify())     
+    return gulp.src(config.srcDir+'/'+config.jsPattern)   
+    .pipe(debug())   
+    .pipe(babel({presets: ["env"]}))
+    .pipe(uglify({ie8:true,warnings:true}))     
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest(config.devDir));
 });
 
