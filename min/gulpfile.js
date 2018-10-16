4bca4f314c23a969da7e4bd4d44da2a8e6888970
Zoviet <ulgrad@inbox.ru>
2018-10-16 16:59:11 +0400

    work 16.10

--- min/gulpfile.js
+++
@@ -1,1 +0,0 @@
-var gulp=require("gulp"),uglify=require("gulp-uglify"),config={srcDir:"",devDir:"./min",jsPattern:"*.js",jsDir:""};gulp.task("js",function(){return gulp.src(config.jsPattern).pipe(uglify()).pipe(gulp.dest(config.devDir))}),gulp.task("default",["js"]);
